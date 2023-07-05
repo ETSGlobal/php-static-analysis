49ed010c548c39fae12dc82fc39a7b8f3f754678
aallier <aallier@eleven-labs.com>
2023-07-05 20:48:53 +0200

    EB-0 Fix get annotation type

--- src/ETSGlobalCodingStandard/Helpers/PropertyHelper.php
+++ src/ETSGlobalCodingStandard/Helpers/PropertyHelper.php
@@ -7,8 +7,9 @@ namespace ETSGlobalCodingStandard\Helpers;
 use PHP_CodeSniffer\Files\File;
 use PHPStan\PhpDocParser\Ast\Type\IntersectionTypeNode;
 use PHPStan\PhpDocParser\Ast\Type\NullableTypeNode;
+use PHPStan\PhpDocParser\Ast\Type\TypeNode;
 use PHPStan\PhpDocParser\Ast\Type\UnionTypeNode;
-use SlevomatCodingStandard\Helpers\Annotation\PropertyAnnotation;
+use SlevomatCodingStandard\Helpers\Annotation;
 use SlevomatCodingStandard\Helpers\AnnotationHelper;
 use SlevomatCodingStandard\Helpers\AnnotationTypeHelper;
 
@@ -23,15 +24,35 @@ class PropertyHelper
         }
 
         // Check PHPDoc type (legacy support)
-        $varAnnotations = AnnotationHelper::getAnnotationsByName($phpcsFile, $propertyPointer, '@var');
-        /** @var PropertyAnnotation|null $propertyAnnotation */
-        $propertyAnnotation = count($varAnnotations) > 0 ? $varAnnotations[0] : null;
+        $varAnnotations = AnnotationHelper::getAnnotations($phpcsFile, $propertyPointer, '@var');
+        /** @var ?Annotation $annotation */
+        $annotation = count($varAnnotations) > 0 ? $varAnnotations[0] : null;
 
-        if ($propertyAnnotation === null) {
+        if ($annotation === null) {
             return null;
         }
 
-        $type = $propertyAnnotation->getType();
+        $type = self::getAnnotationType($annotation);
+
+        if (!$type) {
+            return null;
+        }
+
+        if (AnnotationTypeHelper::containsOneType($type)) {
+            return AnnotationTypeHelper::getTypeHintFromOneType($type);
+        }
+
+        return null;
+    }
+
+    public static function getAnnotationType(Annotation $annotation): ?TypeNode
+    {
+        if (!property_exists($annotation->getValue(), 'type')) {
+            return null;
+        }
+
+        /** @var TypeNode $type */
+        $type = $annotation->getValue()->type;
 
         if (
             $type instanceof IntersectionTypeNode ||
@@ -41,10 +62,6 @@ class PropertyHelper
             return null;
         }
 
-        if (AnnotationTypeHelper::containsOneType($type)) {
-            return AnnotationTypeHelper::getTypeHintFromOneType($type);
-        }
-
-        return null;
+        return $type;
     }
 }
